fb0addcf481d2e8ae808214e2bdf02621e27aa3a
Marcelo Cuellar <mcuellar@buyerzone.com>
2015-08-30 09:57:14 -0400

    DataController implementation for dynamica endpoints.

---
+++ src/Controllers/DataController.cs
@@ -0,0 +1,45 @@
+﻿using InterceuticalsService.Common;
+using InterceuticalsService.Dao;
+using InterceuticalsService.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace InterceuticalsService.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class DataController : ApiController
+    {
+        // Need to DI this way.  Via controller DI did not work.
+        AppDao AppDao = (AppDao)AppContext.GetSpringObject("appDao");
+
+        [HttpGet]
+        public HttpResponseMessage Get(string id)
+        {
+            string errMsg = "FAILED: Unable to get cart totals.";
+            string msg = "Success";
+            string devMsg = "";
+            ResponseResult response = null;
+            List<Hashtable> data = null;
+
+            try
+            {
+                data = AppDao.getDataAsList(id);
+                return Request.CreateResponse<List<Hashtable>>(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+                response = new ResponseResult { Message = errMsg, DeveloperMessage = ex.Message };
+                return Request.CreateResponse<ResponseResult>(HttpStatusCode.InternalServerError, response);
+            }
+
+        }
+    }
+}
---
+++ src/Dao/AppDao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IBatisNet.DataMapper;
+using InterceuticalsService.Models;
+using IBatisNet.DataMapper.Exceptions;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Collections;
+
+namespace InterceuticalsService.Dao
+{
+    public class AppDao
+    {
+        public List<Hashtable> getDataAsList(string name, Dictionary<string,string> inParams = null)
+        {
+            //IList<Hashtable> result = Mapper.Instance().QueryForList<Hashtable>(name, inParams);
+
+            //List<Dictionary<string, string>> results = new List<Dictionary<string, string>>();
+
+            //foreach (var item in result)
+            //    results.Add(HashtableToDictionary(item));
+
+            return (List<Hashtable>) Mapper.Instance().QueryForList<Hashtable>(name, inParams);
+        }
+    }
+}
